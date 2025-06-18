2d0f80bfc9a279c5cdc68e02753f723d13aec8e0
Prasanth Thiagarajan <prasanth33460@gmail.com>
2025-06-19 01:32:25 +0530

    Dockerization done.

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,4 @@
-from app.db.models.user import User
-from app.db.models.project import Project
-from app.db.models.task import Task
-from app.db.models.task_comment import TaskComment
+from app.db.models.user import User #type: ignore
+from app.db.models.project import Project #type: ignore
+from app.db.models.task import Task #type: ignore
+from app.db.models.task_comment import TaskComment #type: ignore
